1f263c26e3e0aad9eb408b6fe70694c060cd243d
Jose David <josedavid.gutierrez@axarnet.dev>
2026-04-07 09:48:28 +0200

    Add: Añadir Filtrado por nombre en la clase author

--- src/BC/Author/Application/Port/AuthorRepositoryPort.php
+++ src/BC/Author/Application/Port/AuthorRepositoryPort.php
@@ -15,7 +15,5 @@ interface AuthorRepositoryPort
 
     public function deleteAuthor(AuthorIdValueObject $id): void;
 
-    public function listAuthors() : array;
-
-    public function filterAuthors(?string $fullname, int $page = 1, int $perPage=10) : array;
+    public function listAuthors(?string $fullName = null, int $page=1, int $perPage=10) : array;
 }
--- src/BC/Author/Infrastructure/Traits/ListAuthorTrait.php
+++ src/BC/Author/Infrastructure/Traits/ListAuthorTrait.php
@@ -6,10 +6,32 @@ use Src\BC\Author\Infrastructure\Hydrators\AuthorHydrators;
 
 trait ListAuthorTrait
 {
-    public function listAuthors(): array
-    {
-        return AuthorModel::all()
-            ->map(fn($model) => AuthorHydrators::toDomain($model))
-            ->toArray();
+    public function listAuthors(?string $fullName = null, int $page = 1, int $perPage = 10): array
+    {   
+        $query = AuthorModel::query();
+
+        if($fullName)
+        {
+            $query->where(function ($q) use ($fullName)
+                    {
+                        $q->where('first_name', 'LIKE', "%{$fullName}%")
+                          ->orWhere('last_name', 'LIKE', "%{$fullName}%");
+                    });
+        }
+
+        $paginator = $query->paginate($perPage, ['*'], 'page', $page);
+
+        return [
+            'items' => array_map(
+                fn($model) => AuthorHydrators::toDomain($model), 
+                $paginator->items()
+            ),
+            'pagination' => [
+                'total'        => $paginator->total(),
+                'perPage'      => $paginator->perPage(),
+                'currentPage'  => $paginator->currentPage(),
+                'lastPage'     => $paginator->lastPage(),
+            ]
+        ];
     }
 }
