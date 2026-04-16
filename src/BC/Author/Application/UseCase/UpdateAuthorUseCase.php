fd0181493058eb0ad5ab9223994ac877b543d484
Jose David <josedavid.gutierrez@axarnet.dev>
2026-04-16 12:26:03 +0200

    Arreglado el Update Author

--- src/BC/Author/Application/UseCase/UpdateAuthorUseCase.php
+++ src/BC/Author/Application/UseCase/UpdateAuthorUseCase.php
@@ -10,6 +10,8 @@ use Src\BC\Author\Domain\ValueObject\AuthorFirstNameValueObject;
 use Src\BC\Author\Domain\ValueObject\AuthorLastNameValueObject;
 use Src\BC\Author\Domain\ValueObject\AuthorBirthDateValueObject;
 use Exception;
+use Src\BC\Author\Domain\ValueObject\AuthorEmailValueObject;
+use Src\BC\Author\Domain\ValueObject\AuthorPasswordValueObject;
 
 class UpdateAuthorUseCase
 {
@@ -27,12 +29,16 @@ class UpdateAuthorUseCase
         $firstName = $dto->getFirstName() ?? $existingAuthor->getAuthorFirstNameValue();
         $lastName = $dto->getLastName() ?? $existingAuthor->getAuthorLastNameValue();
         $birthDate = $dto->getBirthDate() ?? $existingAuthor->getAuthorBirthDateValue();
+        $email = $dto->getEmail() ?? $existingAuthor->getAuthorEmailValue();
+        $password = $dto->getPassword() ?? $existingAuthor->getAuthorPasswordValue();
 
         $author = new Author(
             $id,
             new AuthorFirstNameValueObject($firstName),
             new AuthorLastNameValueObject($lastName),
-            new AuthorBirthDateValueObject($birthDate)
+            new AuthorBirthDateValueObject($birthDate),
+            new AuthorEmailValueObject($email),
+            new AuthorPasswordValueObject($password)
         );
 
         $this->repo->updateAuthor($author);
